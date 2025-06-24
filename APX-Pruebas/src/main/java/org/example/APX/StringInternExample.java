e2964582aa131102047ebb1b3e116464dccce181
Hernan Garcia <hernangava1524@gmail.com>
2025-06-24 00:45:51 -0600

    Primer-Commit

--- APX-Pruebas/src/main/java/org/example/APX/StringInternExample.java
+++ APX-Pruebas/src/main/java/org/example/APX/StringInternExample.java
@@ -2,6 +2,7 @@ package org.example.APX;
 
 public class StringInternExample {
     public static void main(String[] args) {
+        System.out.println(" Pruebas de comparacion de cadenas ");
         String str1 = new String("example");
         String str2 = "example";
         String str3 = str1.intern();

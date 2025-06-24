3ba5cc63a300ad29c37b750ab7dac0f0b827faa7
Hernan Garcia <hernangava1524@gmail.com>
2025-06-24 00:53:50 -0600

    Otra linea que se agrega

--- APX-Pruebas/src/main/java/org/example/APX/StringInternExample.java
+++ APX-Pruebas/src/main/java/org/example/APX/StringInternExample.java
@@ -12,5 +12,7 @@ public class StringInternExample {
         System.out.println(str1 == str2);
         System.out.println(str2 == str3);
         System.out.println(str1 == str3);
+
+        System.out.println(" Finaliza programa *  ");
     }
 }
